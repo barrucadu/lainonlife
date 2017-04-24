775058eb18bf882402b7a8556bba4db916fa3606
Michael Walker <mike@barrucadu.co.uk>
2017-04-24 07:37:43 +0100

    Add a radio page.

---
+++ radio.js
@@ -0,0 +1,80 @@
+// The initial channel
+var channel = "random";
+
+function check_status() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+            let response = JSON.parse(httpRequest.responseText);
+            let listeners = 0;
+            let listenersPeak = 0;
+            let artist = "";
+            let title = "";
+
+            // Find the stats for the appropriate output.
+            for(id in response.icestats.source) {
+                let source = response.icestats.source[id];
+
+                // Assume that the listeners of the ogg and mp3
+                // streams are disjoint and just add them.  Bigger
+                // numbers are better, right?
+                if(source.server_name.startsWith(channel + " (")) {
+                    listeners     += source.listeners;
+                    listenersPeak += source.listener_peak;
+                }
+                // For some reason the mp3 output has mangled unicode.
+                // Probably something to do with how MPD is
+                // transcoding, so only get the artist and title from
+                // the ogg stream.
+                if(source.server_name == channel + " (ogg)") {
+                    artist = source.artist;
+                    title = source.title;
+                }
+            }
+
+            // Update the stats on the page.
+            document.getElementById("nowplaying").innerText = artist + " - " + title;
+            document.getElementById("listeners").innerText  = listeners + " (peak: " + listenersPeak + ")";
+        };
+    };
+
+    httpRequest.open("GET", "/radio/status-json.xsl");
+    httpRequest.send();
+}
+
+function change_channel(e) {
+    let audio  = document.getElementById("audio");
+    let paused = audio.paused;
+
+    // Update the channel
+    channel = e.value;
+
+    // Use either the ogg or mp3 stream, depending on what the current one is.
+    if(audio.currentSrc.endsWith("ogg")) {
+        audio.currentSrc = "/radio/" + channel + ".ogg";
+    } else {
+        audio.currentSrc = "/radio/" + channel + ".mp3";
+    }
+
+    // Load the new audio stream.
+    audio.load();
+
+    // Start playing, if it was before.
+    if(!paused) {
+        audio.play();
+    }
+
+    // Update the stream links.
+    document.getElementById("ogglink").src = "/radio/" + channel + ".ogg";
+    document.getElementById("mp3link").src = "/radio/" + channel + ".mp3";
+
+    // Update the status.
+    check_status();
+}
+
+// Hide the javascript warning.
+document.getElementById("noscript").style.display = "none";
+
+// Get the initial status and set a timer to regularly update it.
+check_status();
+setInterval(check_status, 15000);
