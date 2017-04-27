55df786093500502de0f9a54b8ebf1d2bbaa433a
Michael Walker <mike@barrucadu.co.uk>
2017-04-27 06:20:17 +0100

    Add a page to upload short voice-overs.

--- misc/backend.py
+++ misc/backend.py
@@ -2,19 +2,51 @@
 
 # Backend services.
 
-from flask import Flask, send_file
-import os, random, sys
+from flask import Flask, request, send_file
+import os, random, sys, time
 
 app = Flask(__name__)
 
-@app.route("/background")
+
+def random_file_from(dname):
+    """Serve a random file from a directory."""
+
+    fname = random.choice(os.listdir(dname))
+    return send_file(os.path.join(dname, fname), cache_timeout = 0)
+
+
+@app.route("/background", methods=["GET"])
 def background():
-    fname = random.choice(os.listdir("/srv/http/backgrounds/"))
-    return send_file("/srv/http/backgrounds/{}".format(fname), cache_timeout = 0)
+    return random_file_from("/srv/http/backgrounds")
+
+
+@app.route("/transition.mp3", methods=["GET"])
+def transition():
+    return random_file_from("/srv/radio/music/transitions")
+
+
+@app.route("/upload/voice", methods=["POST"])
+def upload_voice():
+    fname = str(time.time())
+
+    if "file" in request.files:
+        f = request.files["file"]
+        if f and f.filename:
+            f.save(os.path.join("/srv/http/upload", fname + "-file"))
+
+    if "url" in request.form:
+        u = request.form["url"]
+        if u:
+            with open(os.path.join("/srv/http/upload", fname + "-url"), "w") as f:
+                f.write(u)
+
+    return send_file("/srv/http/thankyou.html")
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return send_file("/srv/http/404.html")
+
 
 if __name__ == "__main__":
     try:
