9b1367c99619af68c8a4a14e0c10fa15b0c9e9dd
Michael Walker <mike@barrucadu.co.uk>
2017-04-27 04:41:50 +0100

    Populate the channel list in radio.html with javascript.

--- radio.js
+++ radio.js
@@ -1,51 +1,80 @@
 // The initial channel
 var channel = "everything";
 
-function check_status() {
+function ajax_with_status_json(func) {
     let httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
             let response = JSON.parse(httpRequest.responseText);
-            let listeners = 0;
-            let listenersPeak = 0;
-            let artist = "";
-            let title = "";
-            let description = "";
-
-            // Find the stats for the appropriate output.
-            for(id in response.icestats.source) {
-                let source = response.icestats.source[id];
-
-                // Assume that the listeners of the ogg and mp3
-                // streams are disjoint and just add them.  Bigger
-                // numbers are better, right?
-                if(source.server_name.startsWith(channel + " (")) {
-                    listeners     += source.listeners;
-                    listenersPeak += source.listener_peak;
-                }
-                // For some reason the mp3 output has mangled unicode.
-                // Probably something to do with how MPD is
-                // transcoding, so only get the artist and title from
-                // the ogg stream.
-                if(source.server_name == channel + " (ogg)") {
-                    artist = source.artist;
-                    title  = source.title;
-                    description = source.server_description;
-                }
-            }
-
-            // Update the stats on the page.
-            document.getElementById("nowplaying").innerText = artist + " - " + title;
-            document.getElementById("listeners").innerText  = listeners + " (peak: " + listenersPeak + ")";
-
-            // Update the channel description, in case it's changed.
-            document.getElementById("description1").innerText = description;
-            document.getElementById("description2").innerText = description;
+            func(response);
         };
     };
 
     httpRequest.open("GET", "/radio/status-json.xsl");
     httpRequest.send();
+}
+
+function populate_channel_list() {
+    ajax_with_status_json(function(response) {
+        // Get the list of channels, excluding "everything".
+        let channels = [];
+        for(id in response.icestats.source) {
+            let source = response.icestats.source[id];
+            let sname = source.server_name.substr(0, source.server_name.length - 6);
+            if(source.server_name.endsWith(" (ogg)") && sname != "everything") {
+                channels.push(sname);
+            }
+        }
+
+        // Sort them.
+        channels.sort();
+
+        // Add to the selector drop-down.
+        let dropdown = document.getElementById("channel");
+        for(id in channels) {
+            let channel = channels[id];
+            dropdown.options[dropdown.options.length] = new Option(channel, channel);
+        }
+    });
+}
+
+function check_status() {
+    ajax_with_status_json(function(response) {
+        let listeners = 0;
+        let listenersPeak = 0;
+        let artist = "";
+        let title = "";
+        let description = "";
+
+        // Find the stats for the appropriate output.
+        for(id in response.icestats.source) {
+            let source = response.icestats.source[id];
+
+            // Assume that the listeners of the ogg and mp3 streams
+            // are disjoint and just add them.  Bigger numbers are
+            // better, right?
+            if(source.server_name.startsWith(channel + " (")) {
+                listeners     += source.listeners;
+                listenersPeak += source.listener_peak;
+            }
+            // For some reason the mp3 output has mangled unicode.
+            // Probably something to do with how MPD is transcoding,
+            // so only get the artist and title from the ogg stream.
+            if(source.server_name == channel + " (ogg)") {
+                artist = source.artist;
+                title  = source.title;
+                description = source.server_description;
+            }
+        }
+
+        // Update the stats on the page.
+        document.getElementById("nowplaying").innerText = artist + " - " + title;
+        document.getElementById("listeners").innerText  = listeners + " (peak: " + listenersPeak + ")";
+
+        // Update the channel description, in case it's changed.
+        document.getElementById("description1").innerText = description;
+        document.getElementById("description2").innerText = description;
+    });
 }
 
 function change_channel(e) {
@@ -86,6 +115,9 @@ let show = document.getElementsByClassName("withscript");
 let hide = document.getElementsByClassName("noscript");
 for(let i = 0; i < show.length; i++) { show[i].style.display = (show[i].tagName == "DIV") ? "block" : "inline"; }
 for(let i = 0; i < hide.length; i++) { hide[i].style.display = "none"; }
+
+// Populate the channel list.
+populate_channel_list();
 
 // Get the initial status and set a timer to regularly update it.
 check_status();
