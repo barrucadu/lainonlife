43b8b528c16366bfba3961963d4bfe5a99a5085c
Michael Walker <mike@barrucadu.co.uk>
2017-04-27 05:56:45 +0100

    Produce a directory list as well as a file list.

--- misc/file-list.sh
+++ misc/file-list.sh
@@ -2,6 +2,8 @@
 
 cd /srv/radio/music
 for channel in *; do
-  tree -l                               "$channel" > "/srv/http/file-list/$channel.txt"
-  tree -l -H "" -T "$channel" --nolinks "$channel" > "/srv/http/file-list/$channel.html"
+  tree -l -I transitions                               "$channel" > "/srv/http/file-list/$channel.txt"
+  tree -l -I transitions -H "" -T "$channel" --nolinks "$channel" > "/srv/http/file-list/$channel.html"
+  tree -d -l -I transitions                               "$channel" > "/srv/http/file-list/$channel-albums.txt"
+  tree -d -l -I transitions -H "" -T "$channel" --nolinks "$channel" > "/srv/http/file-list/$channel-albums.html"
 done
